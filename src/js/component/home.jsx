f808e9e8675e0c89f04a5e4c16a38f94009f6c3d
Kei3108 <128880307+Kei3108@users.noreply.github.com>
2023-07-18 05:05:06 +0000

    Finish

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,73 @@
-import React from "react";
+import React, { useState } from 'react';
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  const [inputValue, setInputValue] = useState("");
+  const [hobbies, setHobbies] = useState([]);
+  const [activeIndex, setActiveIndex] = useState(null);
+
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const handleKeyPress = (e) => {
+    if (e.key === "Enter" && inputValue !== "") {
+      setHobbies((prevHobbies) => [...prevHobbies, inputValue]);
+      setInputValue("");
+    }
+  };
+
+  const handleDeleteHobby = (index) => {
+    setHobbies((prevHobbies) => {
+      const updatedHobbies = [...prevHobbies];
+      updatedHobbies.splice(index, 1);
+      return updatedHobbies;
+    });
+  };
+
+  const handleMouseEnter = (index) => {
+    setActiveIndex(index);
+  };
+
+  const handleMouseLeave = () => {
+    setActiveIndex(null);
+  };
+
+  return (
+    <div className="container">
+      <h1>My Hobbies</h1>
+      <ul>
+        <li>
+          <input
+            type="text"
+            onChange={handleInputChange}
+            onKeyPress={handleKeyPress}
+            value={inputValue}
+            placeholder="What needs to be done?"
+          />
+        </li>
+        {hobbies.length === 0 ? (
+          <li>No hobbies. Add hobbies</li>
+        ) : (
+          hobbies.map((hobby, index) => (
+            <li
+              key={index}
+              onMouseEnter={() => handleMouseEnter(index)}
+              onMouseLeave={handleMouseLeave}
+            >
+              {hobby}
+              {activeIndex === index && (
+                <i
+                  className="fas fa-trash-alt"
+                  onClick={() => handleDeleteHobby(index)}
+                ></i>
+              )}
+            </li>
+          ))
+        )}
+      </ul>
+      <div>{hobbies.length} Hobbies</div>
+    </div>
+  );
 };
 
 export default Home;
